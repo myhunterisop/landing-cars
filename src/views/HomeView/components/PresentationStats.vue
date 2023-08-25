226f550a3c825caa38ff672bf88ea389743039ce
u.borovkov <u.borovkov@3h.ru>
2023-08-26 01:17:31 +0300

    presentation animation add

--- src/views/HomeView/components/PresentationStats.vue
+++ src/views/HomeView/components/PresentationStats.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="presentation-stats">
         <div class="row">
-            <div class="col-default-6">
+            <div class="col-default-6 js-animation">
                 <h2 class="with-decor">Наши достижения от кубка россии до чемпионатов мира</h2>
                 <p class="presentation-stats__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Habitant tincidunt aliquam tincidunt pretium sodales. Tristique odio pellentesque et tincidunt posuere arcu purus lobortis risus. Urna, ut amet odio orci magnis praesent ultrices. Praesent malesuada lacus tellus tristique sit at. Sed viverra nulla nam arcu, non iaculis pretium, volutpat. </p>
             </div>
@@ -19,6 +19,44 @@
     </div>
 </template>
 <script setup>
+import { onMounted, nextTick } from 'vue';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {
+    // eslint-disable-next-line no-unused-vars
+    window.addEventListener('scroll', e => {
+        document.body.style.cssText += `--scrollTop: ${window.scrollY}px`
+    })
+
+    nextTick(() => {
+        animationPresentationText()
+    })
+})
+
+const animationPresentationText = () => {
+    const list = gsap.utils.toArray('.js-animation');
+
+    list.forEach((item, index) => {
+        gsap.fromTo(item,
+            {
+                opacity: 0,
+                x: index % 2 ? 70 : -70
+            },
+            {
+                opacity: 1,
+                x: 0,
+                scrollTrigger: {
+                    start: 'top bottom-=100',
+                    end: 'top center',
+                    trigger: item,
+                    scrub: true,
+                }
+            }
+        );
+    })
+}
 </script>
 
 <style lang="scss">
